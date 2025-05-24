df3cc48fcbf3c66f17184359c8f9061cd33615df
Avirup Ghosh <oviirup@gmail.com>
2025-05-24 22:40:01 +0530

    ✨ added basic scraping functionality.
    ‼️ missing scraped data validation, url sanitization, and some key details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import * as React from 'react'
+import Image from 'next/image'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { getSiteMetadata } from '~/actions/scraper'
 import { Button } from '~/components/ui/button'
 import { Icon } from '~/components/ui/icon'
 import { Input } from '~/components/ui/input'
@@ -14,12 +17,37 @@ const zFormSchema = z.object({
 })
 type FormSchema = z.infer<typeof zFormSchema>
 
+const zMetadata = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  image: z.string().url().optional(),
+  url: z.string().url().optional(),
+})
+type Metadata = z.infer<typeof zMetadata>
+
 export default function HomePage() {
+  const [metadata, setMetadata] = React.useState<Metadata>({})
+
   const form = useForm<FormSchema>({
     mode: 'onBlur',
     resolver: zodResolver(zFormSchema),
     defaultValues: { url: '' },
   })
+
+  const handleFormSubmit = async (data: FormSchema) => {
+    setMetadata({})
+    // Handle form submission
+    console.log('Form submitted:', data)
+    const res = await getSiteMetadata(data.url)
+    if (!res.error) {
+      const parsedMetadata = zMetadata.safeParse(res)
+      if (parsedMetadata.success) {
+        setMetadata(parsedMetadata.data)
+      } else {
+        console.error('Error parsing metadata:', parsedMetadata.error)
+      }
+    }
+  }
 
   return (
     <main className="container mx-auto flex grow flex-col items-center justify-center gap-12">
@@ -30,7 +58,10 @@ export default function HomePage() {
         </p>
       </hgroup>
 
-      <form className="w-full max-w-md" noValidate>
+      <form
+        className="w-full max-w-md"
+        noValidate
+        onSubmit={form.handleSubmit(handleFormSubmit)}>
         <Label htmlFor="url" className="ml-1">
           Enter a URL
         </Label>
@@ -66,10 +97,31 @@ export default function HomePage() {
 
       <div className="flex w-full max-w-md flex-col gap-6">
         <div className="flex-1 space-y-2">
-          <Skeleton className="aspect-video bg-input" />
-          <Skeleton className="h-4 w-[78%] bg-input" />
-          <Skeleton className="h-4 w-[51%] bg-input" />
-          <Skeleton className="h-4 w-[44%] bg-input" />
+          {metadata.image ? (
+            <Image
+              src={metadata.image}
+              alt={metadata.title || 'Preview image'}
+              width={448}
+              height={252}
+              unoptimized
+              className="aspect-video w-full rounded-md border bg-input object-cover"
+            />
+          ) : (
+            <Skeleton className="aspect-video bg-input" />
+          )}
+          {metadata.title ? (
+            <h4 className="mx-1 text-base font-medium">{metadata.title}</h4>
+          ) : (
+            <Skeleton className="line-clamp-2 h-4 w-[78%] bg-input" />
+          )}
+          {metadata.description ? (
+            <p className="mx-1 line-clamp-3 text-sm">{metadata.description}</p>
+          ) : (
+            <>
+              <Skeleton className="h-4 w-[51%] bg-input" />
+              <Skeleton className="h-4 w-[44%] bg-input" />
+            </>
+          )}
         </div>
       </div>
     </main>
